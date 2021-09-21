b60e21c4ae167fa25fa443ede8fe444806d6b0e3
raquel <raquelanaroman@gmail.com>
2021-09-21 19:53:57 -0400

    create schema

---
+++ db/schema.rb
@@ -0,0 +1,117 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_21_190425) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "teacher_id", null: false
+    t.bigint "student_id"
+    t.boolean "all_day"
+    t.datetime "start"
+    t.datetime "end"
+    t.time "start_time"
+    t.time "end_time"
+    t.date "start_recur"
+    t.date "end_recur"
+    t.string "title"
+    t.boolean "allow_registration"
+    t.integer "type"
+    t.integer "attendance"
+    t.boolean "visible"
+    t.string "location"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_id"], name: "index_events_on_student_id"
+    t.index ["teacher_id"], name: "index_events_on_teacher_id"
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.bigint "studio_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["studio_id"], name: "index_families_on_studio_id"
+  end
+
+  create_table "jwt_blacklists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp"
+    t.index ["jti"], name: "index_jwt_blacklists_on_jti"
+  end
+
+  create_table "parents", force: :cascade do |t|
+    t.string "secondary_phone"
+    t.boolean "primary_contact"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_parents_on_family_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "grade"
+    t.boolean "adult"
+    t.integer "make_up_credits"
+    t.integer "status"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_students_on_family_id"
+  end
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.bigint "teacher_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teacher_id"], name: "index_studios_on_teacher_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "instrument"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "address"
+    t.string "role_type"
+    t.bigint "role_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_type", "role_id"], name: "index_users_on_role"
+  end
+
+  add_foreign_key "events", "students"
+  add_foreign_key "events", "teachers"
+  add_foreign_key "families", "studios"
+  add_foreign_key "parents", "families"
+  add_foreign_key "students", "families"
+  add_foreign_key "studios", "teachers"
+end
