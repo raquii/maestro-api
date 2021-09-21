eedbe2bef5978af0be649ce4f19facc3ba3d75f4
raquel <raquelanaroman@gmail.com>
2021-09-21 19:53:42 -0400

    add devise logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  scope :api, defaults: {format: :json} do
+    devise_for :users,
+      path: '',
+      path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'sessions',
+        registrations: 'registrations'
+      }
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
