df5c9739ef52000ceaa11e0ebda589f7e6406b09
raquel <raquelanaroman@gmail.com>
2021-09-22 12:37:54 -0400

    reorder associations for clarity

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,8 +1,10 @@
 class Teacher < ApplicationRecord
     has_one :user, as: :role, dependent: :destroy
+    
     has_one :studio, dependent: :destroy
-    has_many :families, through: :studio
     has_many :events, dependent: :destroy
+
+    has_many :families, through: :studio
     has_many :students, through: :studio
     has_many :parents, through: :studio
     
