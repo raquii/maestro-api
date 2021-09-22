7a18cda34406bd437cca252338a0f1444bddb981
raquel <raquelanaroman@gmail.com>
2021-09-22 12:37:39 -0400

    add scope to user for role associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
   belongs_to :role, polymorphic: true
+
+  scope :teachers, -> {where(role_type: "Teacher")}
+  scope :students, -> {where(role_type: "Student")}
+  scope :parents, -> {where(role_type: "Parent")}
 end
