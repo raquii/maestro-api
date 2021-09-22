b675a85f0ba7b705579b6d856f710b1da8f984c4
raquel <raquelanaroman@gmail.com>
2021-09-22 12:38:10 -0400

    add enumerators

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :teacher, dependent: :destroy
-    belongs_to :student, :optional
+    belongs_to :student, optional: true
+
+    enum type: [:lesson, :make_up, :recital, :vacation, :other]
+    enum attendance: [:present, :absent_notice, :absent_no_notice, :late, :teacher_absent]
+
 end
