d8542776648e7126f49a8200d5fb55bf236b36c4
raquel <raquelanaroman@gmail.com>
2021-09-23 11:30:16 -0400

    add student_profile assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,13 @@ class User < ApplicationRecord
 
   belongs_to :family, optional: true
   has_one :studio, foreign_key: :teacher_id, dependent: :destroy
+  has_one :student_profile, dependent: :destroy
+
   has_many :children, through: :family
   has_many :students, through: :studio
+
+  has_many :events, foreign_key: :teacher_id, dependent: :destroy
+  has_many :events_as_student, through: :student_profile
 
   enum role: [:teacher, :student, :guardian, :admin]
 end
