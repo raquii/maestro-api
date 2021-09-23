94e4288b3801fdc52ec85f1b808f39b8f9fe51b0
raquel <raquelanaroman@gmail.com>
2021-09-23 11:30:55 -0400

    update associations

--- app/models/family.rb
+++ app/models/family.rb
@@ -3,5 +3,6 @@ class Family < ApplicationRecord
     has_many :students, -> { where(role: 1) }, class_name: :User
     has_many :children, -> { where(role: 1) }, class_name: :User
     has_many :parents, -> { where(role: 2) }, class_name: :User
-    has_many :events, through: :students
+    
+    has_many :events_as_student, through: :students
 end
