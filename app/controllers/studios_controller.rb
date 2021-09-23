19d5ae40ae0cebf82bb425d2d8bf703bee87a543
raquel <raquelanaroman@gmail.com>
2021-09-23 11:31:08 -0400

    create show and update methods

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,22 @@
 class StudiosController < ApplicationController
+    def show
+        studio = find_studio
+        render json: studio
+    end
+
+    def update
+        studio = find_studio
+        studio.update!(studio_params)
+        render json: studio
+    end
+
+    private
+
+    def studio_params
+        params.permit(:name
+    end
+
+    def find_studio
+        Studio.find(params[:id])
+    end
 end
