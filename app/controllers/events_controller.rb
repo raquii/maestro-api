9733cfdd94e82a060a3895dc3ed7f2ee9237d9e9
raquel <raquelanaroman@gmail.com>
2021-09-24 22:58:58 -0400

    events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
 class EventsController < ApplicationController
+
+    def current_ability
+        @current_ability ||= EventAbility.new(current_user)
+    end
+
+    def index
+       
+    end
 end
