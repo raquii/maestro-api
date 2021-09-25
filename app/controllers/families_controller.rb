885bda8f4671a92d4d11bef95f2f871d48bfc428
raquel <raquelanaroman@gmail.com>
2021-09-24 22:58:29 -0400

    add route methods for families

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,35 @@
 class FamiliesController < ApplicationController
+
+    def index
+        families = current_user.studio.families
+        render json: families
+    end
+
+    def show
+        family = find_family
+        render json: family
+    end
+
+    def update
+        family = find_family
+        family.update!(family_params)
+        render json: family
+    end
+
+    def destroy
+        family = find_family
+        name = family.name
+        family.destroy
+        render json: {message: "#{name} Family has been deleted."}
+    end
+
+    private
+
+    def find_family
+        Family.find(params[:id])
+    end
+
+    def family_params
+        params.permit(:name)
+    end
 end
