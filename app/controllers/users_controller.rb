73b1c2c43f61b81c130a31931847882aa8334707
raquel <raquelanaroman@gmail.com>
2021-09-24 22:59:44 -0400

    add methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
     skip_authorization_check
+    skip_before_action :authenticate_user!, only: :index
+
+    def current_ability
+        @current_ability ||= UserAbility.new(current_user)
+    end
+
     def index
         users = User.all
         render json: users
+    end
+
+    def show
+        user = current_user
+        render json: user
+    end
+
+    def update
+
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:email, :first_name, :last_name, :phone, :address, :password)
     end
 end
