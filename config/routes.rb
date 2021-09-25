d02d98870d3560919dd91a5d74529b54892b027c
raquel <raquelanaroman@gmail.com>
2021-09-24 23:00:00 -0400

    change sign_up route path

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
     path_names: {
       sign_in: 'login',
       sign_out: 'logout',
-      registration: 'signup'
+      registration: 'sign_up'
     },
     controllers: {
       sessions: 'sessions',
       registrations: 'registrations'
     }, 
     defaults: { format: :json }
+    get '/me', to: 'users#show'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
