3cc7dc0f24551e966d3ef8cce722aafaa632573c
raquel <raquelanaroman@gmail.com>
2021-09-24 22:58:14 -0400

    add permitted params to sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::MimeResponds
     respond_to :json
-
+    before_action :authenticate_user!
     check_authorization unless: :devise_controller?
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :password, :password_confirmation, :first_name, :last_name, :address, :phone, :role, :family_id])
+    end
     
 end
