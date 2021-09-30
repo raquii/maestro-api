62f8c87a86265991c7e29ba3ee14cbcdb61ce812
raquel <raquelanaroman@gmail.com>
2021-09-29 21:46:52 -0400

    fix student serializer

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -2,11 +2,7 @@ class StudentSerializer
     include JSONAPI::Serializer
     set_key_transform :camel_lower
 
-    attributes :first_name
+    attributes :first_name, :last_name, :phone, :email, :address
+    attribute :student_profile, serializer: StudentProfileSerializer
 
-    attribute :student_profile, if: Proc.new { |record, params|
-        record.student?
-    } do |object|
-        StudentProfileSerializer.new(object.student_profile)
-    end
 end
