6cdede2b484347c8706aa9070fb94f0fff957396
raquel <raquelanaroman@gmail.com>
2021-09-30 14:39:38 -0400

    fix cancancan logic, add missing methods

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,25 +1,42 @@
 class StudiosController < ApplicationController
-    load_and_authorize_resource
     
     def current_ability
         @current_ability ||= StudioAbility.new(current_user)
     end
 
+    def index
+        authorize! :index, Studio
+        @studios = Studio.accessible_by(current_ability)
+        render json: StudioSerializer.new(@studios)
+    end
+
     def show
-        studio = find_studio
-        render json: studio
+        @studio = find_studio
+        authorize! :show, @studio
+        render json: StudioSerializer.new(@studio)
+    end
+
+    def create
+        @studio = Studio.new(studio_params)
+        
+        if can? :create, @studio
+            @studio.save!
+            render json: StudioSerializer.new(@studio), status: :created
+        end 
     end
 
     def update
-        studio = find_studio
-        studio.update!(studio_params)
-        render json: studio
+        @studio = find_studio
+        if can? :edit, @studio
+            @studio.update!(studio_params)
+            render json: StudioSerializer.new(@studio)
+        end
     end
 
     private
 
     def studio_params
-        params.permit(:name
+        params.permit(:name, :teacher_id)
     end
 
     def find_studio
