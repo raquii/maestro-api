4114bbaaf62f51b75fb9795c119ed46cade9dd59
raquel <raquelanaroman@gmail.com>
2021-09-29 21:46:38 -0400

    update event ability conditions

--- app/abilities/event_abilities.rb
+++ app/abilities/event_abilities.rb
@@ -1,13 +1,14 @@
 class EventAbility
-    include CanCan::UserAbility
+    include CanCan::Ability
     def initialize(user)
         
         return unless user.present?
-        can :read, Event, visible: true, teacher: user.family.studio.teacher
-        can :update, Event, allow_registration: true, teacher: user.family.studio.teacher, :student
-        can :update, Event, student: {student_profile: {user: user} }, :student
+        can :read, Event, visible: true, teacher: user.family.teacher
+        can :update, Event, allow_registration: true, teacher: user.family.teacher, :student
+        can :update, Event, student: user, :student
+        
         return unless user.guardian?
-        can :update, Event, student: {student_profile: {user: {family: user.family}} }, :student
+        can :update, Event, student: {family: user.family}, :student
 
         return unless user.teacher?
         can :manage, Event, teacher: user
