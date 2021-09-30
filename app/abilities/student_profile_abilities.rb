2a1c82490f8e84e87e1485520b65f550487aaac5
raquel <raquelanaroman@gmail.com>
2021-09-29 21:47:27 -0400

    update ability logic

--- app/abilities/student_profile_abilities.rb
+++ app/abilities/student_profile_abilities.rb
@@ -1,15 +1,15 @@
 class StudentProfileAbility
-    include CanCan::UserAbility
+    include CanCan::Ability
     def initialize(user)
         
-        return unless user.present?``
-        can [:read,:update], StudentProfile, user: user
+        return unless user.present?
+        can :read, StudentProfile, user: user
 
         return unless user.guardian?
-        can [:read,:update], StudentProfile, user: {family: user.family}
+        can :read, StudentProfile, user: {family: user.family}
 
         return unless user.teacher?
-        can :manage, StudentProfile, user:{family:{studio: user.studio}}
+        can :manage, StudentProfile, user:{family:{teacher: user}}
 
         return unless user.admin?
         can :manage, StudentProfile
