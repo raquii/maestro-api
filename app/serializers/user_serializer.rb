b4f6596691d5e4cc06dc5d865c95d581df0e4230
raquel <raquelanaroman@gmail.com>
2021-09-29 21:45:23 -0400

    clean up serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,41 +5,13 @@ class UserSerializer
 
   attributes :first_name, :last_name, :phone, :address, :role, :email
 
-  attribute :studio, if: Proc.new { |record, params|
+  attribute :studio, if: Proc.new { |record|
     record.teacher?
   } do |object|
     StudioSerializer.new(object.studio)
   end
 
-  attribute :student_profile, if: Proc.new { |record, params|
-    record.student?
-  } do |object|
-    StudentProfileSerializer.new(object.student_profile)
+  attribute :teacher, if: Proc.new{|record| record.family } do |object|
+    TeacherSerializer.new(object.family.teacher)
   end
-
-  attribute :students, if: Proc.new { |record, params|
-    record.teacher?
-  } do |object|
-    UserSerializer.new(object.students)
-  end
-
-  attribute :children, if: Proc.new { |record, params|
-    record.guardian?
-  } do |object|
-    UserSerializer.new(object.children)
-  end
-
-  attribute :events do |object|
-    if object.teacher?
-      EventSerializer.new(object.students)
-    elsif object.student?
-      EventSerializer.new(object.events_as_student)
-    else
-      EventSerializer.new(object.get_family_events)
-    end
-  end
-
-
-
-
 end
