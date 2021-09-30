c063cdc114404ccb6847abbb6b0683ce21572ef6
raquel <raquelanaroman@gmail.com>
2021-09-30 14:40:11 -0400

    fix cancancan auth logic in controller

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,26 +1,45 @@
 class FamiliesController < ApplicationController
 
+    def current_ability
+        @current_ability ||= FamilyAbility.new(current_user)
+    end
+
     def index
-        families = current_user.studio.families
-        render json: families
+        authorize! :index, Family
+        @families = Family.accessible_by(current_ability)
+        render json: FamilySerializer.new(@families)
     end
 
     def show
-        family = find_family
-        render json: family
+        @family = find_family
+        authorize! :show, @family
+        render json: FamilySerializer.new(@family)
+    end
+
+    def create
+        @family = Family.new(studio_params)
+        
+        if can? :create, @family
+            @family.save!
+            render json: FamilySerializer.new(@family), status: :created
+        end
     end
 
     def update
-        family = find_family
-        family.update!(family_params)
-        render json: family
+        @family = find_family
+        if can? :edit, @family
+            @family.update!(family_params)
+            render json: FamilySerializer.new(@family)
+        end
     end
 
     def destroy
-        family = find_family
-        name = family.name
-        family.destroy
-        render json: {message: "#{name} Family has been deleted."}
+        @family = find_family
+        if can? :destroy, @family
+            name = @family.name
+            @family.destroy
+            render json: {message: "#{name} Family has been deleted."}
+        end
     end
 
     private
