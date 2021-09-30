d9c71aa5bead6f9a5ca688448928b74ca9c01f7b
raquel <raquelanaroman@gmail.com>
2021-09-30 14:39:55 -0400

    add missing methods with proper cancancan logic

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,4 @@
 class StudentsController < ApplicationController
-    load_and_authorize_resource :user, parent: false
 
     def current_ability
         @current_ability ||= UserAbility.new(current_account).merge(StudentProfileAbility.new(current_user))
@@ -7,13 +6,51 @@ class StudentsController < ApplicationController
     
     def index
         if current_user.teacher?
-            students = current_user.students
+            @students = current_user.students
         elsif current_user.guardian?
-            students = current_user.children
+            @students = current_user.children
         end
-        render json: StudentSerializer.new(students)
+
+        if can? :index, @students
+            render json: StudentSerializer.new(@students)
+        end
     end
 
-    def show
+    def create
+        @student = User.new(student_params)
+
+        if can? :create, @student
+            @student.save!
+            render json: StudentSerializer.new(@student), status: :created
+        end
     end
+
+    def update
+        @student = find_student
+
+        if can? :edit, @student
+            @student.update!(student_params)
+            render json: StudentSerializer.new(@student)
+        end
+    end
+
+    def destroy
+        @student = find_student
+        if can? :destroy, @student
+            @student.destroy
+            render json: {message: "User deleted"}
+        end
+    end
+    
+    private 
+
+    def student_params
+        params.permit(:first_name, :last_name, :email, :phone, :address, :family_id, :role)
+    end
+
+    def find_student
+        User.find(params[:id])
+    end
+
+    
 end
