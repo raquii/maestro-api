8accc836f36c485b90489dbcd7334cd7aacf312a
raquel <raquelanaroman@gmail.com>
2021-09-30 14:40:38 -0400

    remove check_auth causing issue on log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,29 @@ class ApplicationController < ActionController::API
     include ActionController::MimeResponds
     respond_to :json
     before_action :authenticate_user!
-    check_authorization unless: :devise_controller?
-
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_response_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_response_invalid
     
     protected
+
+    rescue_from CanCan::AccessDenied do |exception|
+      respond_to do |format|
+        format.json { render json: {message: 'Not Found'}, status: :not_found }
+      end
+    end
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :password, :password_confirmation, :first_name, :last_name, :address, :phone, :role, :family_id])
+    end
+
+    def render_response_not_found
+      render json: { errors: "Not found."}, status: :not_found
+    end
+
+    def render_response_invalid(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
     
 end
