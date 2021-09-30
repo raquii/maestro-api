b244a0d3e67d0ba77dc740ccd8a02b04bcb01b04
raquel <raquelanaroman@gmail.com>
2021-09-29 21:45:49 -0400

    add attributes to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,5 +2,7 @@ class EventSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
   
-  attributes :id
+  attributes :all_day, :start, :end, :start_time, :end_time, :start_recur, :end_recur, :title, :allow_registration, :type, :attendance, :visible, :location, :price
+  belongs_to :teacher, record_type: :user
+  belongs_to :student, record_type: :user
 end
