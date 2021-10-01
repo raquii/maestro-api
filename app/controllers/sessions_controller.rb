69505a0bc9816b965f61826a6ac823bf06c81de2
raquel <raquelanaroman@gmail.com>
2021-10-01 14:02:27 -0400

    update session to respond with current user obj

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < Devise::SessionsController
     private
     
     def respond_with(resource, _opts = {})
-      render json: { message: 'You are logged in.' }, status: :ok
+      render json: UserSerializer.new(current_user), status: :ok
     end
   
     def respond_to_on_destroy
