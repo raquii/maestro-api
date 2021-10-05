630dfa891cd8002c932f18584d796e48828279c7
raquel <raquelanaroman@gmail.com>
2021-10-05 18:34:01 -0400

    add enum to status property

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -2,4 +2,5 @@ class StudentProfile < ApplicationRecord
     belongs_to :user
 
     has_many :events_as_student, class_name: :Event, foreign_key: :student_id, dependent: :nullify
+    enum status: [:active, :lead, :waiting, :trial, :inactive]
 end
