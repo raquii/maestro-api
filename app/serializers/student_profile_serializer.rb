42ac19e52fe4ab842d4324e014be9b68b6eeb1fb
raquel <raquelanaroman@gmail.com>
2021-10-05 13:38:28 -0400

    fix bug where profile serializer wasn't being used

--- app/serializers/student_profile_serializer.rb
+++ app/serializers/student_profile_serializer.rb
@@ -2,5 +2,5 @@ class StudentProfileSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
 
-  attributes :grade, :make_up_credits, :status, :adult, :user_id
+  attributes :grade, :make_up_credits, :status, :adult, :user_id, :default_lesson_duration, :default_lesson_price
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -3,6 +3,8 @@ class StudentSerializer
     set_key_transform :camel_lower
 
     attributes :first_name, :last_name, :phone, :email, :address
-    attribute :student_profile, serializer: StudentProfileSerializer
+    attribute :student_profile do |object| 
+        StudentProfileSerializer.new(object.student_profile)
+    end
 
 end
