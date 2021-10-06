af569a6c0918c62735be5dbf988bc02711658c76
raquel <raquelanaroman@gmail.com>
2021-10-05 23:21:04 -0400

    add new attributes to serializer

--- app/serializers/student_profile_serializer.rb
+++ app/serializers/student_profile_serializer.rb
@@ -2,5 +2,5 @@ class StudentProfileSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
 
-  attributes :grade, :make_up_credits, :status, :adult, :user_id, :default_lesson_duration, :default_lesson_price
+  attributes :grade, :make_up_credits, :status, :adult, :user_id, :default_lesson_duration, :default_lesson_price, :birthday, :gender
 end
