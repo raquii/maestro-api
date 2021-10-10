c96d46efafe1d4e2b2edba4562ca3cb7132ecd2b
raquel <raquelanaroman@gmail.com>
2021-10-10 17:30:01 -0400

    add update to preferences controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,2 +1,18 @@
 class PreferencesController < ApplicationController
+    
+    def update
+        preference = current_user.preference
+        preference.update(preference_params)
+        render json: PreferenceSerializer.new(preference)
+    end
+
+    private
+
+    def find_preference
+        Preference.find(params[:id])
+    end
+
+    def preference_params
+        params.permit(:cancellation_deadline, :permit_cancellations, :permit_event_registration, :event_registration_deadline, :permit_make_up_credits, :issue_make_up_credit_before_deadline, :expire_make_up_credits, :max_credit_age, :limit_total_make_up_credits, :max_total_make_up_credits, :cancellation_policy_summary, :default_event_visibility, :default_lesson_price, :default_lesson_duration, :initial_view, :slot_min_time, :slot_max_time, :weekends, :location, :students_can_edit_profile)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :preferences
+    resources :preferences, only: :update
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
