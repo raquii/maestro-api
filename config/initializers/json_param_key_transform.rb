6588c4b0e1a0c1f7e32fc0c3dc17623b46dc2123
raquel <raquelanaroman@gmail.com>
2021-10-09 20:03:44 -0400

    change param nesting key

--- config/initializers/json_param_key_transform.rb
+++ config/initializers/json_param_key_transform.rb
@@ -3,7 +3,6 @@
 ActionDispatch::Request.parameter_parsers[:json] = lambda { |raw_post|
     # Modified from action_dispatch/http/parameters.rb
     data = ActiveSupport::JSON.decode(raw_post)
-  
     # Transform camelCase param keys to snake_case
     if data.is_a?(Array)
       data.map { |item| item.deep_transform_keys!(&:underscore) }
@@ -12,5 +11,5 @@ ActionDispatch::Request.parameter_parsers[:json] = lambda { |raw_post|
     end
   
     # Return data
-    data.is_a?(Hash) ? data : { '_json': data }
+    data.is_a?(Hash) ? data : { 'data': data }
 }
