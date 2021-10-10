dd081e34983cb650bea6587fe16b3e2afe2f488b
raquel <raquelanaroman@gmail.com>
2021-10-09 20:08:37 -0400

    try to rescue from cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::API
     protected
 
     rescue_from CanCan::AccessDenied do |exception|
+      Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
       respond_to do |format|
         format.json { render json: {message: 'Not Found'}, status: :not_found }
       end
