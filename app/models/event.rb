bba340a293f717211b843eea3a73590cda981686
raquel <raquelanaroman@gmail.com>
2021-10-14 15:44:14 -0400

    update event enum

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     belongs_to :teacher, class_name: :User, dependent: :destroy
     belongs_to :student, class_name: :User, optional: true
 
-    enum type: [:lesson, :make_up, :recital, :vacation, :other]
+    enum type: [:lesson, :group_lesson, :make_up_lesson, :recital, :vacation, :birthday]
     enum attendance: [:present, :absent_notice, :absent_no_notice, :late, :teacher_absent]
 
 end
