05bb16fe44860adb09271a203edf2206ad6c5d5f
raquel <raquelanaroman@gmail.com>
2021-10-15 10:23:23 -0400

    add defaults to all preferences

--- db/migrate/20211006202952_create_preferences.rb
+++ db/migrate/20211006202952_create_preferences.rb
@@ -9,18 +9,18 @@ class CreatePreferences < ActiveRecord::Migration[6.1]
       t.boolean :permit_make_up_credits, default: true
       t.boolean :issue_make_up_credit_before_deadline, default: true
       t.boolean :expire_make_up_credits, default: false
-      t.integer :max_credit_age
+      t.integer :max_credit_age, default: 0
       t.boolean :limit_total_make_up_credits, default: true
       t.integer :max_total_make_up_credits, default: 4
-      t.text :cancellation_policy_summary
+      t.text :cancellation_policy_summary, default: ""
       t.boolean :default_event_visibility, default: true
-      t.integer :default_lesson_price
-      t.integer :default_lesson_duration
+      t.integer :default_lesson_price, default: 0
+      t.integer :default_lesson_duration, default: 0
       t.integer :initial_view, default: 0
       t.string :slot_min_time, default:"08:00"
       t.string :slot_max_time, default:"21:00"
       t.boolean :weekends, default: true
-      t.string :location
+      t.string :location, default: ""
       t.boolean :students_can_edit_profile, default: true
 
     end
