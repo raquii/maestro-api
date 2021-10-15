bacbf9ba9ed3fd08b5cb8f07ea3c2c90a177a396
raquel <raquelanaroman@gmail.com>
2021-10-15 10:26:30 -0400

    add serialization for new columns

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,7 +2,9 @@ class EventSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
   
-  attributes :all_day, :start, :end, :start_time, :end_time, :start_recur, :end_recur, :title, :allow_registration, :type, :attendance, :visible, :location, :price
-  belongs_to :teacher, record_type: :user
-  belongs_to :student, record_type: :user
+  attributes :teacher_id, :student_id, :all_day, :start, :end, :start_time, :end_time, :start_recur, :end_recur, :title, :allow_registration, :event_type, :attendance, :visible, :location, :price, :make_up_credit_required, :default_lesson
+
+  attribute :days_of_week do |object|
+    object.days_of_week.digits()
+  end
 end
--- app/serializers/preference_serializer.rb
+++ app/serializers/preference_serializer.rb
@@ -2,5 +2,6 @@ class PreferenceSerializer
     include JSONAPI::Serializer
     set_key_transform :camel_lower
   
-    attributes :cancellation_deadline, :permit_cancellations, :permit_event_registration, :event_registration_deadline, :permit_make_up_credits, :issue_make_up_credit_before_deadline, :expire_make_up_credits, :max_credit_age, :limit_total_make_up_credits, :max_total_make_up_credits, :cancellation_policy_summary, :default_event_visibility, :default_lesson_price, :default_lesson_duration, :initial_view, :slot_min_time, :slot_max_time, :weekends, :location, :students_can_edit_profile
+    attributes :cancellation_deadline, :permit_cancellations, :permit_event_registration, :event_registration_deadline, :permit_make_up_credits, :issue_make_up_credit_before_deadline, :expire_make_up_credits, :max_credit_age, :limit_total_make_up_credits, :max_total_make_up_credits, :cancellation_policy_summary, :default_event_visibility, :default_lesson_price, :default_lesson_duration, :initial_view, :slot_min_time, :slot_max_time, :weekends, :location, :students_can_edit_profile, :lesson_color, :group_lesson_color, :recital_color, :make_up_lesson_color, :vacation_color, :birthday_color
+
   end
