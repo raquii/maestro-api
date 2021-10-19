3cc0e91757a5fcee7b6aabe81fbe28a35751f50e
raquel <raquelanaroman@gmail.com>
2021-10-18 20:33:06 -0400

    overhaul student controller logic

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,114 +1,108 @@
 class StudentsController < ApplicationController
-  
-    def current_ability
-        @current_ability ||= UserAbility.new(current_user).merge(StudentProfileAbility.new(current_user))
-    end
-    
-    def index
 
-        if current_user.teacher?
-            @students = current_user.students
-        elsif current_user.guardian?
-            @students = current_user.children
-        end
-      
-        render json: StudentSerializer.new(@students)
+    def index
+        @students = StudentProfile.accessible_by(current_ability)
+        render json: StudentProfileSerializer.new(@students)
     end
 
     def create
- 
-        @student = User.new(user_params(:student))
-
-        @student.studio = current_user.studio_as_teacher
-        @student.build_student_profile(profile_params)
-
-        if params[:student][:family_id].blank?
-            @family = Family.new()
-            @student.family = @family
-        else
-            @family = Family.find(params[:family_id])
-            @student.family = @family
-        end
-
-        unless params[:guardian1][:first_name].blank?
-            byebug
-            @guardian1 = User.new(user_params(:guardian1))
-            @guardian1.studio = @student.studio
-            @guardian1.family = @student.family
-        end
-     
-        unless params[:guardian2][:first_name].blank?
-            byebug
-            @guardian2 = User.new(user_params(:guardian2))
-            @guardian2.studio = @student.studio
-            @guardian2.family = @student.family
-        end
+        @student = StudentProfile.new(student_profile_params)
 
         if can? :create, @student
-            byebug
+            if params[:student_profile][:family_id].blank?
+                @family = Family.new(studio:current_user.studio)
+                @student.family = @family
+            else
+                @family = Family.find(params[:family_id])
+                @student.family = @family
+            end
+            
+            @family.save!
             @student.save!
-            render json: StudentSerializer.new(@student), status: :created
+           
+            unless params[:guardian_profile_a][:first_name].blank?
+                @guardian_a = GuardianProfile.new(guardian_profile_params(:guardian_profile_a))
+                @guardian_a.family = @family
+                @guardian_a.save!
+            end
+     
+            unless params[:guardian_profile_b][:first_name].blank?
+                @guardian_b = GuardianProfile.new(guardian_profile_params(:guardian_profile_b))
+                @guardian_b.family = @family
+                @guardian_b.save!
+            end
+
+            render json: StudentProfileSerializer.new(@student), status: :created
         end
     end
 
     def update
-        @student = find_student
+        @student = find_student_profile
 
         authorize! :edit, @student
         @student.update!(student_params)
-        render json: StudentSerializer.new(@student)
+        render json: StudentProfileSerializer.new(@student)
     end
 
     def destroy
-        @student = find_student
+        @student = find_student_profile
+        @id = @student.id
+
         if can? :destroy, @student
-            @student.destroy
-            render json: {message: "User deleted"}
+            family = student.family
+            if family.students.count <= 1
+                family.destroy!
+            else
+                @student.destroy!
+            end
+            render json: {id: @id}
         end
     end
 
     def bulk_update        
         students = find_students
-        profiles = find_student_profile
-      
-        if students.all?{|s| can? :update, s}
-            profiles.update_all(status: params[:students][:status])       
-            render json: StudentSerializer.new(students)
+      if students.all?{|s| can? :update, s}
+            if params[:status]
+                students.update_all(status:params[:status])  
+            else
+                students.update_all(make_up_credits:params[:make_up_credits])  
+            end     
+            render json: StudentProfileSerializer.new(students)
         end
     end
 
     def bulk_destroy
-        students = find_student
+        students = find_students
+
         if students.all?{|s| can? :destroy, s}
-            students.destroy_all
-            render json: {message: "Successfuly deleted."}
+            students.each do |student|
+                family = student.family
+                if family.students.count <= 1
+                    family.destroy!
+                else
+                    student.destroy!
+                end
+            end
+            render json: StudentProfileSerializer.new(current_user.students)
         end
     end
     
     private 
 
-    def user_params(symbol)
-        params.require(symbol).permit(:id, :first_name, :last_name, :email, :phone, :address, :family_id, :role, :lesson_reminder_emails, :lesson_reminder_sms)
+    def guardian_profile_params(symbol)
+        params.require(symbol).permit(:id, :first_name, :last_name, :email, :phone, :address, :family_id, :studio_id, :lesson_reminder_emails, :lesson_reminder_sms)
     end
 
-    def student_params
-        params.require(:student).permit(:first_name, :last_name, :email, :phone, :address, :family_id, :role, :lesson_reminder_emails, :lesson_reminder_sms)
-    end
-
-    def profile_params
-        params.require(:student_profile).permit(:grade, :make_up_credits, :status, :adult, :student, :student_id, :default_lesson_duration, :default_lesson_price, :birthday, :gender, :school)
-    end
-
-    def find_student
-        User.find(params[:id])
-    end
-
-    def find_students
-        User.where(id: params[:students][:ids])
+    def student_profile_params
+        params.require(:student_profile).permit(:id, :first_name, :last_name, :email, :phone, :address, :family_id, :studio_id, :grade, :adult, :make_up_credits, :status, :school, :notes,:default_lesson_duration, :default_lesson_price, :birthday, :gender, :lesson_reminder_emails, :lesson_reminder_sms)
     end
 
     def find_student_profile
-       StudentProfile.where(id: params[:students][:profile_ids])
-    end    
+        StudentProfile.find(params[:id])
+    end
+
+    def find_students
+        StudentProfile.where(id: params[:ids])
+    end
     
 end
