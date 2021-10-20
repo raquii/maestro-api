f5e3e98187b0bc514da95a51213c9b3b7edc258f
raquel <raquelanaroman@gmail.com>
2021-10-19 21:07:18 -0400

    add recurring event methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
 
     def create
         @event = Event.new(event_params)
+        @event.teacher_profile = current_user.teacher_profile
 
         if can? :create, @event
             @event.save!
@@ -21,7 +22,23 @@ class EventsController < ApplicationController
         end
     end
 
+    def create_recurring_event
+        @group = RecurringGroup.new(recurring_group_params)
+        @event = Event.new(recurring_event_params)
+        @event.teacher_profile = current_user.teacher_profile
+
+        if can? :create, @event
+            @group.save!
+            @event.recurring_group=@group
+            @event.save!
+            @event.generate_recurrent_events
+            @recurrent_events = @group.events.limit(30)
+            render json: EventSerializer.new(@recurrent_events)
+        end
+    end
+
     def update
+        byebug
         @event = find_event
         if can? :edit, @event
             @event.update!(event_params)
@@ -29,12 +46,48 @@ class EventsController < ApplicationController
         end
     end
 
+    def update_all_event_occurances
+        byebug
+        @events = find_all_occurances
+        byebug
+        if @events
+            render json: {message: 'got em'}
+        end
+    end
+
+    def update_future_event_occurances
+        byebug
+        @events = find_future_occurances
+        byebug
+        if @events
+            render json: {message: 'got em'}
+        end
+    end
+
     def destroy
         @event = find_event
         if can? :destroy, @event
-            title = @event.title
+            id = @event.id
             @event.destroy
-            render json: {message: "#{title} has been deleted."}
+            render json: {id: id, message: "event deleted"}
+        end
+    end
+
+    def destroy_all_event_occurances
+        byebug
+        @events = find_all_occurances
+        byebug
+        if @events
+            render json: {message: 'got em'}
+        end
+    end
+
+    def destroy_future_event_occurances
+        byebug
+        @events = find_future_occurances
+        byebug
+        if @events
+            render json: {message: 'got em'}
         end
     end
 
@@ -44,7 +97,23 @@ class EventsController < ApplicationController
         Event.find(params[:id])
     end
 
+    def find_all_occurances
+        Event.where({recurring_group_id: params[:event][:recurring_group_id]})
+    end
+
+    def find_future_occurances
+        Event.where({recurring_group_id: params[:event][:recurring_group_id]}).where('start >= ?', params[:event][:start])
+    end
+
     def event_params
-        params.permit(:teacher_id, :student_id, :all_day, :start, :end, :start_time, :end_time, :start_recur, :end_recur, :title, :allow_registration, :event_type, :attendance, :visible, :location, :price, :make_up_credit_required, :default_lesson, :days_of_week)
+        params.permit(:teacher_profile_id, :student_profile_id, :all_day, :start, :end, :recurring_group_id, :title, :allow_registration, :event_type, :attendance, :visible, :location, :price, :make_up_credit_required, :default_lesson, recurring_group:[:end_date])
+    end
+
+    def recurring_event_params
+        params.require(:event).permit(:teacher_profile_id, :student_profile_id, :all_day, :start, :end, :recurring_group_id, :title, :allow_registration, :event_type, :attendance, :visible, :location, :price, :make_up_credit_required, :default_lesson)
+    end
+
+    def recurring_group_params
+        params.require(:recurring_group).permit(:end_date)
     end
 end
