911c49cb1ec42ae14ae003996107ecc8f5932812
raquel <raquelanaroman@gmail.com>
2021-10-21 13:06:20 -0400

    add second digit to price attr

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,10 +2,14 @@ class EventSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
   
-  attributes :teacher_profile_id, :student_profile_id, :all_day, :start, :end, :title, :allow_registration, :event_type, :attendance, :visible, :location, :price, :recurring_group_id, :make_up_credit_required, :default_lesson
+  attributes :teacher_profile_id, :student_profile_id, :all_day, :start, :end, :title, :allow_registration, :event_type, :attendance, :visible, :location, :recurring_group_id, :make_up_credit_required, :default_lesson
 
   attribute :recurring do |object|
     object.recurring_group.present?
+  end
+
+  attribute :price do |object|
+    ActionController::Base.helpers.number_with_precision(object.price, precision: 2)
   end
 
   attribute :end_date, if: Proc.new{ |record| record.recurring_group.present? } do |object|
