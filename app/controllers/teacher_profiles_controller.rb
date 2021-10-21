fae23076caa8e036e2b1a0d5da96d698d1348425
raquel <raquelanaroman@gmail.com>
2021-10-21 13:04:53 -0400

    add update method

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,2 +1,8 @@
 class TeacherProfilesController < ApplicationController
+    def update
+        @teacher = current_user.teacher_profile
+        if can? :update, @teacher
+            @teacher.update(teacher_profile_params)
+        end
+    end
 end
