717cedfadf66b00433290e1be64d59f413dc1b16
raquel <raquelanaroman@gmail.com>
2021-10-21 13:05:58 -0400

    avoid excessive nesting from fastJson gem

--- app/serializers/student_profile_serializer.rb
+++ app/serializers/student_profile_serializer.rb
@@ -5,7 +5,15 @@ class StudentProfileSerializer
   attributes :first_name, :last_name, :phone, :email, :address, :grade, :make_up_credits, :status, :adult, :default_lesson_duration, :default_lesson_price, :birthday, :gender, :school, :notes, :lesson_reminder_emails, :lesson_reminder_sms
 
   attribute :guardians, if: Proc.new{|record| record.guardians.present? } do |object|
-    GuardianProfileSerializer.new(object.guardians)
+    object.guardians.each do |g|
+      {
+      guardian_profile_id: g.id,
+      first_name: g.first_name,
+      last_name: g.last_name,
+      email: g.email,
+      phone: g.phone
+    }
+    end
   end
 
 end
