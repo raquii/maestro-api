e5913ee355cd947a3b514da91b12afc9a9a5ac38
raquel <raquelanaroman@gmail.com>
2021-10-21 13:05:17 -0400

    create add birthday to calendar method

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,8 @@
+### TO DO ###
+# make birthday creation a recurring job
+#
+
+
 class StudentProfile < ApplicationRecord
     belongs_to :user, inverse_of: :student_profile, optional: :true
     belongs_to :family, inverse_of: :students
@@ -8,4 +13,24 @@ class StudentProfile < ApplicationRecord
 
     enum status: [:active, :lead, :waiting, :trial, :inactive]
 
+    after_create :add_birthday_to_calendar
+
+    def add_birthday_to_calendar
+        if self.birthday.present?
+            birthday = self.birthday
+            year = Date.current.year
+            group = RecurringGroup.create()
+            Event.create(
+                title: "#{self.first_name} #{self.last_name}'s Birthday!", 
+                event_type: 5, 
+                start: birthday.change(year:year), 
+                all_day:true, 
+                teacher_profile:self.teacher, 
+                student_profile:self,
+                visible: true,
+                allow_registration: false,
+                recurring_group: group
+            )
+        end
+    end
 end
