6848dcecb7b093f74430db2bdd1ed7021edce5f8
raquel <raquelanaroman@gmail.com>
2021-10-26 12:49:37 -0400

    fix dependent macro with events

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -9,7 +9,7 @@ class StudentProfile < ApplicationRecord
     belongs_to :studio, inverse_of: :students, optional: true
     has_one :teacher, through: :studio, source: :teacher_profile
     has_many :guardians, through: :family, source: :guardians
-    has_many :events, inverse_of: :student_profile
+    has_many :events, inverse_of: :student_profile, dependent: :nullify
 
     enum status: [:active, :lead, :waiting, :trial, :inactive]
 
