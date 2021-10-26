6b641aaa4eab4e8cceacc6ab7d4f780728212f9b
raquel <raquelanaroman@gmail.com>
2021-10-26 12:50:08 -0400

    add method to update teacher profile

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,8 +1,15 @@
 class TeacherProfilesController < ApplicationController
     def update
-        @teacher = current_user.teacher_profile
+        byebug
+        @teacher = TeacherProfile.find(params[:id])
         if can? :update, @teacher
             @teacher.update(teacher_profile_params)
         end
+    end
+
+    private
+
+    def teacher_profile_params
+        params.permit(:first_name, :last_name, :email, :phone, :address, studio_attributes:[:name])
     end
 end
